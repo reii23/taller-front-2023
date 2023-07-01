33cb3d1cc9da3f94d09261e3135b61bc89914273
Reinaldo Pacheco <reinaldo.pacheco@usach.cl>
2023-07-01 17:20:30 -0400

    actualización 01/07

---
+++ miapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header/index.js';
+import Navbar from './components/Navbar/index.js';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      {/* Agrega el resto del contenido de tu aplicación aquí */}
+    </div>
+  );
+};
+
+export default App;
---
+++ miapp/src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
+
